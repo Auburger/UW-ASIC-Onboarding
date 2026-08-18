f95576bf0abb1cdc9495c8c5b6ba78796bb8a9e7
Auburger <aubriechan07@gmail.com>
2026-08-17 22:39:54 -0400

    Fixed a syntax error

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -69,7 +69,7 @@ module spi_peripheral (
                 7'h02 : en_reg_pwm_7_0 <= data[7:0];
                 7'h03 : en_reg_pwm_15_8 <= data[7:0];
                 7'h04 : pwm_duty_cycle <= data[7:0];
-                default : // do nothing if address invalid
+                default : ;// do nothing if address invalid
             endcase
         end 
     end
