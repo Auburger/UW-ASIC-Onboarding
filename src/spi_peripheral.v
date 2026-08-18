5b9ad2623ddb0b3fdd01b664569a7d9140348d85
Auburger <aubriechan07@gmail.com>
2026-08-18 12:02:19 -0400

    Fixed typo

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -1,7 +1,7 @@
 module spi_peripheral (
     input wire clk,
     input wire rst_n,
-    input wire [2:0] ui_in;
+    input wire [2:0] ui_in,
     output reg [7:0] en_reg_out_7_0,
     output reg [7:0] en_reg_out_15_8,
     output reg [7:0] en_reg_pwm_7_0,
@@ -11,7 +11,7 @@ module spi_peripheral (
     wire sclk_in = ui_in[0];
     wire copi_in = ui_in[1];
     wire ncs_in = ui_in[2];
-    
+
     // Clock domain crossing bit:
     reg [1:0] sclk_chain;
     reg [1:0] copi_chain;
